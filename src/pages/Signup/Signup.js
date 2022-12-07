4994434bc3cb42022fa6970fe6344062e69999b6
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-07 16:04:15 +0900

    MODIFY: className camelCase로 전체 수정 및 nesting

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,81 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
   return (
-    <div className="signup__entire-box">
-      <div className="signup__left-side">
-        <div className="signup__header-section">
-          <a className="signup__back-arrow" href="/Main">
+    <div className="entireArray">
+      <div>
+        <header className="headerArray">
+          <a className="arrowMargin" href="/Main">
             -화살표-
           </a>
-          <span className="signup__logo">로고</span>
-        </div>
-        <span className="signup__left-container">
-          <div className="signup__title-section">
-            <div className="signup__title-style">Sims & co 회원 가입</div>
-            <div className="signup__go-to-login">
+          <span className="logoStyle">로고</span>
+        </header>
+        <span className="sideContainerWidth">
+          <div className="titleMargin">
+            <h1 className="titleStyle">Sims & co 회원 가입</h1>
+            <div>
               이미 가입하셨나요? <a href="login">로그인 하기</a>
             </div>
           </div>
-          <div className="signup__img-min" />
-          <div className="signup__img-container">
-            <span className="signup__img-leftside">
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
+          <img
+            className="mobileImgSize"
+            alt="loginImg"
+            src="../../../public/images/Signup/signup_img(2).jpg"
+          />
+          <div className="imgContainerFlex">
+            <span className="imgBox1Margin">
+              <img
+                alt="loginImage1"
+                className="imgItemStyle1"
+                src="../../../public/images/Signup/signup_img(5).jpg"
+              />
             </span>
-            <span className="signup__img-rightside">
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
-              <div className="signup__img-items" />
+            <span className="imgBox2Margin">
+              <img
+                alt="loginImage1"
+                className="imgItemStyle2"
+                src="../../../public/images/Signup/signup_img(5).jpg"
+              />
             </span>
           </div>
         </span>
       </div>
-      <div className="signup__input-container">
+      <div className="inputContainerStyle">
         <div>
-          성
-          <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          이름
-          <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          생일
-          <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          휴대폰 <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          성별(선택 사항)
-          <select className="signup__input-items" name="성별">
-            <option value="" />
-            <option value="남성">남성</option>
-            <option value="여성">여성</option>
-          </select>
-        </div>
-        <div>
-          상세 주소
-          <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          이메일 <input type="text" className="signup__input-items" />
-        </div>
-        <div>
-          비밀번호
-          <input type="password" className="signup__input-items" />
+          <input type="text" className="inputItemStyle" />
         </div>
 
-        <button className="signup__button-style">가입 하기</button>
+        <button className="buttonStyle">가입 하기</button>
       </div>
     </div>
   );
 };
 
 export default Signup;
+
+const SIGNUP_INPUT_BOX = {
+  id: 1,
+  title:
+    '성, 이름, 생일, 휴대폰, 성별(남성, 여성), 상세 주소, 이메일, 비밀번호',
+  type: 'text',
+  className: 'inputItemStyle',
+};
+
+const SIGNUP_IMAGE = [
+  {
+    id: 1,
+    alt: 'image1',
+    src: '../../../public/images/Signup/signup_img(1)',
+  },
+  {
+    id: 2,
+    alt: 'image2',
+    src: '../../../public/images/Signup/signup_img(2)',
+  },
+  {
+    id: 3,
+    alt: 'image3',
+    src: '../../../public/images/Signup/signup_img(3)',
+  },
+  {
+    id: 4,
+    alt: 'image4',
+    src: '../../../public/images/Signup/signup_img(4)',
+  },
+  {
+    id: 5,
+    alt: 'image5',
+    src: '../../../public/images/Signup/signup_img(5)',
+  },
+];
