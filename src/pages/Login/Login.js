4d7f69f8aa70c457b918e2828d027407f065664e
문승훈 <moonseunghun@munseunghun-ui-MacBookPro.local>
2022-12-07 18:06:07 +0900

    Add:로그인 레이아웃 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
   return (
-    <div className="login">
-      <div className="loginLeftBox">
-        <div className="leftBoxTextBox">
-          <div className="leftBoxLogo">
+    <div className="loginContainer">
+      <div className="titleBox">
+        <div className="useBox">
+          <div className="textBox">
             <div className="logoIcon">icon</div>
             <span>Sims & co</span>
           </div>
           <div className="textBoxTitle">
-            <h1>로그인</h1>
+            <h1 className="title">로그인</h1>
             <span>외워야 할 비밀번호가 많아 불편하셨죠?</span>
             <p>이제 일회용 코드를 이용하여 간편하게 로그인하세요.</p>
 
             <p>*이메일 또는 전화번호 최초 인증 후 사용 가능</p>
           </div>
-          <div className="titleDetail">
+          <div className="footer">
             <span>sims&co쿠키 정책,개인정보처리방침</span>
           </div>
         </div>
       </div>
-      <div className="loginRightBox">
-        <div className="rightBoxLogin">
+      <div className="loginRbox">
+        <div className="totalFormBox">
           <form>
             <div className="loginBoard">
               <div className="loginBoardEmail">
                 이메일 또는 확인된 휴대폰 번호
                 <input type="text" className="inputId" />
               </div>
-
               <div className="loginBoardPw">
                 <span className="loginBordPwtitle">비밀번호</span>
-                <input type="text" className="inputPw" />
+                <input
+                  type="password"
+                  className="inputPassword"
+                  name="userPassword"
+                />
               </div>
-
               <button className="loginBtn" type="button">
                 로그인
               </button>
