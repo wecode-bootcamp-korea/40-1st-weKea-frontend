6f005b4584aff450bb0ce5fba91d230990ef0e1e
Taeyooooon <eric568@naver.com>
2022-12-09 14:10:14 +0900

    ADD : Category Layout 구현  - 카테고리 아이템 컴포넌트로 관리  - 카테고리 flex 이용해서 정렬  - 카테고리 필터 sticky 이용해서 일정 구간에서 네비게이션 역할

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import CategoryItem from './CategoryItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './category.scss';
 const Category = () => {
   return (
     <div className="category">
       <div className="categoryTitle">
-        <h1>모든 제품</h1>
+        <h1>카테고리 이름</h1>
       </div>
-      <div className="categoryFilter">필터</div>
-      <div className="categoryList">
-        <CategoryItem price="1" />
-        <CategoryItem price="2" />
-        <CategoryItem price="3" />
-        <CategoryItem price="4" />
-        <CategoryItem price="5" />
-        <CategoryItem price="6" />
-        <CategoryItem price="7" />
-        <CategoryItem price="8" />
+      <div className="buttonCountWrapper">
+        <div className="categoryButton">
+          <button>제품</button>
+          <button>공간</button>
+        </div>
+
+        <span className="categoryCount">123개 항목</span>
+      </div>
+      <div className="categoryWrapper">
+        <div className="categoryFilter">
+          <button>
+            <span>정렬</span>
+            <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
+          </button>
+          <button>
+            <span>가격</span>
+            <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
+          </button>
+          <button>
+            <span>색상</span>
+            <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
+          </button>
+          <button>
+            <span>모든 필터</span>
+            <FontAwesomeIcon icon="fa-solid fa-list-ul" />
+          </button>
+        </div>
+        <div className="categoryList">
+          {/* 테스트용 */}
+          <CategoryItem price="1" />
+          <CategoryItem price="2" />
+          <CategoryItem price="3" />
+          <CategoryItem price="4" />
+          <CategoryItem price="5" />
+          <CategoryItem price="6" />
+          <CategoryItem price="7" />
+          <CategoryItem price="8" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Category/CategoryItem.js
+++ src/pages/Category/CategoryItem.js
@@ -3,27 +3,28 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './categoryItem.scss';
 
-const CategoryItems = props => {
+const CategoryItems = ({ price }) => {
   return (
     <div className="categoryItems">
       <div className="categoryImg">
         <img
           src="https://www.ikea.com/kr/ko/images/products/barnasinne-spice-mill-black-white__1114947_pe871876_s5.jpg?f=m"
           alt="상품이미지"
-          // 임시 사이즈
           width="350px"
           height="350px"
         />
       </div>
+
       <div className="itemInformationWrapper">
         <Link className="itemInformation" to="#">
           <h3 className="itemTitle">
             <span className="itemName">VINTERFINT 빈테르핀트</span>
             <span className="itemDescription">걸이식장식용품8종</span>
           </h3>
-          <span className="itemPrice">{props.price}</span>
+          <span className="itemPrice">{price}</span>
           <span className="itemStar">상품별점</span>
         </Link>
+
         <button className="iconCart">
           <FontAwesomeIcon icon="fa-solid fa-cart-shopping" size="lg" />
         </button>
