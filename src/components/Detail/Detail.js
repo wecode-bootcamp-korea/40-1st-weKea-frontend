698e3779b754f9c0557c829acb3fb4edcb8b548f
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-14 11:08:56 +0900

    MODIFY: 리뷰 반영

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -4,21 +4,9 @@ import './Detail.scss';
 
 const Detail = () => {
   const [detailImageList, setDetailImagelist] = useState([]);
-
-  const productDetail = {
-    id: 1,
-    french_name: 'CHOUCHOU',
-    korean_name: '슈슈',
-    category: '식탁',
-    material: '호두나무',
-    size: '180x90cm',
-    price: 590000,
-    rating: '★★★★★(15)',
-    description_summary:
-      '원목으로 만든 기본 원형 테이블과 의자는 공간을 따뜻하고 포근하게 만들어 줍니다. 프랑스 가정집의 감성을 멋지게 표현했죠.',
-    product_code: '110.530.87',
-    description_detail:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ligula nibh. Aenean fringilla sem vel tellus hendrerit, in varius lectus blandit. Maecenas nec iaculis odio. Quisque eget orci mollis, faucibus nulla vitae, interdum nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt condimentum odio, sit amet ultricies quam tincidunt at. Ut nec ultrices diam. Quisque molestie maximus turpis ac dignissim. Suspendisse vel risus sed nibh iaculis auctor nec sit amet justo.',
+  const [detailInfoList, setDetailInfoList] = useState([]);
+  const numberWithCommas = x => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   };
 
   useEffect(() => {
@@ -28,6 +16,16 @@ const Detail = () => {
       .then(res => res.json())
       .then(data => {
         setDetailImagelist(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch('/data/productDetail.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setDetailInfoList(...data);
       });
   }, []);
 
@@ -47,45 +45,39 @@ const Detail = () => {
             );
           })}
         </ul>
-        <div className="detailSummary">{productDetail.description_summary}</div>
+        <div className="detailSummary">
+          {detailInfoList.description_summary}
+        </div>
         <div className="productCodeBox">
           <div className="productCodeTitle">제품 번호</div>
-          <span className="productCode">{productDetail.product_code}</span>
+          <span className="productCode">{detailInfoList.product_code}</span>
         </div>
         <ul className="detailList">
-          <li className="detailListItem">
-            제품 설명
-            <span className="detailModalArrow">
-              <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
-            </span>
-          </li>
-          <li className="detailListItem">
-            치수
-            <span className="detailModalArrow">
-              <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
-            </span>
-          </li>
-          <li className="detailListItem">
-            상품평
-            <span className="detailModalArrow">
-              <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
-            </span>
-          </li>
+          {DETAIL_LIST.map(data => {
+            return (
+              <li key={data.id} className="detailListItem">
+                {data.title}{' '}
+                <span className="detailModalArrow">
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+              </li>
+            );
+          })}
         </ul>
       </main>
 
       <aside className="productBuyModule">
         <div className="stickyContainer">
           <div className="productNameBox">
-            <span className="productName">{productDetail.french_name}</span>
-            <span className="productName">{productDetail.korean_name}</span>
+            <span className="productName">{detailInfoList.french_name}</span>
+            <span className="productName">{detailInfoList.korean_name}</span>
           </div>
-          <div className="subDescription">
-            {productDetail.category}, {productDetail.material},{' '}
-            {productDetail.size}
+          <div className="subDescription">{detailInfoList.category}</div>
+          <div className="price">
+            <div className="currencyStyle">&#8361;</div>
+            {numberWithCommas(+detailInfoList.price)}
           </div>
-          <div className="price">&#8361;{productDetail.price}</div>
-          <div className="subRating">{productDetail.rating}</div>
+          <div className="subRating">{detailInfoList.rating}</div>
           <div className="buyMethodContainer">
             <div className="buyMethodMsg">어떻게 구매하시겠어요?</div>
             <div className="buyMethodBox">
@@ -109,3 +101,9 @@ const Detail = () => {
 };
 
 export default Detail;
+
+const DETAIL_LIST = [
+  { id: 1, title: '제품 설명' },
+  { id: 2, title: '치수' },
+  { id: 3, title: '상품평' },
+];
