dae935e1bb46615e6fd109524cb95dd6d8ab1940
Hyobin Bae <amy@Hyobinui-MacBookPro.local>
2022-12-15 16:28:06 +0900

    MODIFY: API config 파일로 정리

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = 'http://10.58.52.109:3000';
-
-export const endpoint = { signup: `${BASE_URL}/signup` };
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
 const BASE_URL = 'http://10.58.52.109:3000';
 const API = {
   login: `${BASE_URL}/login`,
+  signup: `${BASE_URL}/signup`,
+  detail: `${BASE_URL}/detail`,
 };
 
 export { API, BASE_URL };
